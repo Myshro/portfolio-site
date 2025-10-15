ec25c953d488adf7226ff5b931ce362e070cccfa
Myshro <greenfishfungus2@gmail.com>
2025-10-14 22:57:35 -0400

    update name

--- components/page.tsx
+++ components/page.tsx
@@ -41,7 +41,7 @@ export default function Portfolio() {
       <div id="about" className="max-w-6xl mx-auto px-4 py-20 flex items-center justify-between">
         <div className="max-w-2xl">
           <h1 className="text-6xl font-bold mb-6">
-            Andrew Jungtaek Hong
+            Andrew Hong
           </h1>
           <p className="text-gray-400 text-xl mb-8">
             Hi! I&apos;m currently a second year at the
