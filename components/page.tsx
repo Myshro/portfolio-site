5db589a4426a84cf8ad486ada74a42a03c02f5b5
Myshro <83293772+Myshro@users.noreply.github.com>
2026-04-05 16:42:48 -0400

    fin2

--- components/page.tsx
+++ components/page.tsx
@@ -1,33 +1,8 @@
 'use client';
 
-import { Github, Linkedin, Mail } from 'lucide-react'
-import { Button } from "@/components/ui/button"
-import { Card } from "@/components/ui/card"
-import ProjectCard from './ProjectCard'
-import { useEffect, useState } from 'react';
-
 export default function Portfolio() {
   return (
     <p></p>
   )
 }
 
-const quotes: string[] = [
-  "Do what you can, with what you have, where you are",
-  "Blessed are the curious, for they shall have adventures",
-  "The greater the difficulty, the more glory in surmounting it"
-];
-
-const RandomQuote = () => {
-  const [quote, setQuote] = useState('');
-
-  useEffect(() => {
-    setQuote(quotes[Math.floor(Math.random() * quotes.length)]);
-  }, []);
-
-  return (
-    <p className="text-gray-500 text-l mt-8 italic">
-      &ldquo;{quote}&rdquo;
-    </p>
-  );
-};
